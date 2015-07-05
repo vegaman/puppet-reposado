20658c89e2acf935e350a0be6fdeaf3dfedcfc90
Gerard Kok <gerard@gerardkok.nl>
2015-07-05 17:04:40 +0200

    debugging

--- lib/puppet/parser/functions/rewrite_rules.rb
+++ lib/puppet/parser/functions/rewrite_rules.rb
@@ -42,7 +42,7 @@ def catalog_url(os_name)
 end
 
 def rewrite_condition(os_name)
-  darwin_version = os_minor_version(version).to_i + 4
+  darwin_version = (os_minor_version(version).to_i + 4).to_s
   "%{HTTP_USER_AGENT} Darwin/#{darwin_version}"
 end
 
