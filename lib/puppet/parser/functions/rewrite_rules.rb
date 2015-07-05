d86a84fd82cfbaf8b9acd1808d15ebe99a76c817
Gerard Kok <gerard@gerardkok.nl>
2015-07-05 17:20:32 +0200

    debugging

--- lib/puppet/parser/functions/rewrite_rules.rb
+++ lib/puppet/parser/functions/rewrite_rules.rb
@@ -1,79 +1,78 @@
-def os_minor_version(os_name)
-  case os_name
-  when 'leopard', '10.5'
-    5
-  when 'snowleopard', '10.6'
-    6
-  when 'lion', '10.7'
-    7
-  when 'mountainlion', '10.8'
-    8
-  when 'mavericks', '10.9'
-    9
-  when 'yosemite', '10.10'
-    10
-  else
-    11
-  end
-end
-
-def os_name_in_catalog_url(os_minor_version)
-  case os_minor_version
-  when 5
-    'leopard'
-  when 6
-    'snowleopard'
-  when 7
-    'lion'
-  when 8
-    'mountainlion'
-  when 9
-    '10.9'
-  when 10
-    '10.10'
-  else
-    '10.11'
-  end
-end
-
-def darwin_version(os_name)
-  case os_name
-  when 'leopard', '10.5'
-    9
-  when 'snowleopard', '10.6'
-    10
-  when 'lion', '10.7'
-    11
-  when 'mountainlion', '10.8'
-    12
-  when 'mavericks', '10.9'
-    13
-  when 'yosemite', '10.10'
-    14
-  else
-    15
-  end
-end
-
-def catalog_url(os_name)
-  os_minor_versions = (5..os_minor_version(os_name)).to_a.reverse
-  url = os_minor_versions.map { |o| os_name_in_catalog_url(o) }.join('-')
-  "/content/catalogs/others/index-#{url}.merged-1"
-end
-
-def rewrite_rule(os_name)
-  catalog_url = catalog_url([os_name])
-  "^/index(.*)\.sucatalog$ #{catalog_url}$1.sucatalog [L]"
-end
-
-def rewrite(os_name)
-  Hash.new['rewrite_cond',"",'rewrite_rule',""]
-end
+#def os_minor_version(os_name)
+#  case os_name
+#  when 'leopard', '10.5'
+#    5
+#  when 'snowleopard', '10.6'
+#    6
+#  when 'lion', '10.7'
+#    7
+#  when 'mountainlion', '10.8'
+#    8
+#  when 'mavericks', '10.9'
+#    9
+#  when 'yosemite', '10.10'
+#    10
+#  else
+#    11
+#  end
+#end
+#
+#def os_name_in_catalog_url(os_minor_version)
+#  case os_minor_version
+#  when 5
+#    'leopard'
+#  when 6
+#    'snowleopard'
+#  when 7
+#    'lion'
+#  when 8
+#    'mountainlion'
+#  when 9
+#    '10.9'
+#  when 10
+#    '10.10'
+#  else
+#    '10.11'
+#  end
+#end
+#
+#def darwin_version(os_name)
+#  case os_name
+#  when 'leopard', '10.5'
+#    9
+#  when 'snowleopard', '10.6'
+#    10
+#  when 'lion', '10.7'
+#    11
+#  when 'mountainlion', '10.8'
+#    12
+#  when 'mavericks', '10.9'
+#    13
+#  when 'yosemite', '10.10'
+#    14
+#  else
+#    15
+#  end
+#end
+#
+#def catalog_url(os_name)
+#  os_minor_versions = (5..os_minor_version(os_name)).to_a.reverse
+#  url = os_minor_versions.map { |o| os_name_in_catalog_url(o) }.join('-')
+#  "/content/catalogs/others/index-#{url}.merged-1"
+#end
+#
+#def rewrite_rule(os_name)
+#  catalog_url = catalog_url([os_name])
+#  "^/index(.*)\.sucatalog$ #{catalog_url}$1.sucatalog [L]"
+#end
+#
+#def rewrite(os_name)
+#  Hash.new['rewrite_cond',"",'rewrite_rule',""]
+#end
 
 module Puppet::Parser::Functions
   newfunction(:rewrite_rules, :type => :rvalue) do |args|
     info("args[0]: #{args[0]}")
-    os_names = args[0]
     []
   end
 end
