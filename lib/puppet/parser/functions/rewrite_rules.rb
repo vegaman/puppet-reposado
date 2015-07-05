d60e2c2266b46afbc83c8f1522fcd7f8eeae1e24
Gerard Kok <gerard@gerardkok.nl>
2015-07-05 16:49:25 +0200

    debugging

--- lib/puppet/parser/functions/rewrite_rules.rb
+++ lib/puppet/parser/functions/rewrite_rules.rb
@@ -1,11 +1,53 @@
+def os_minor_version(os_name)
+  case os_name
+  when 'leopard', '10.5'
+    5
+  when 'snowleopard', '10.6'
+    6
+  when 'lion', '10.7'
+    7
+  when 'mountainlion', '10.8'
+    8
+  when 'mavericks', '10.9'
+    9
+  when 'yosemite', '10.10'
+    10
+  else
+    11
+  end
+end
+
+def os_name_in_catalog_url(os_minor_version)
+  case os_minor_version
+  when 5
+    'leopard'
+  when 6
+    'snowleopard'
+  when 7
+    'lion'
+  when 8
+    'mountainlion'
+  when 9
+    '10.9'
+  when 10
+    '10.10'
+  else
+    '10.11'
+  end
+end
+
+def catalog_url(os_name)
+  os_minor_versions = (5..os_minor_version(os_name)).to_a.reverse
+  '/content/catalogs/others/index-' + os_minor_versions.map { |o| os_name_in_catalog_url(o) }.join('-') + '.merged-1'
+end
+
 def rewrite_condition(os_name)
-  os_minor_version = function_os_minor_version([os_name])
-  darwin_version = os_minor_version + 4
+  darwin_version = os_minor_version(version) + 4
   "%{HTTP_USER_AGENT} Darwin/#{darwin_version}"
 end
 
 def rewrite_rule(os_name)
-  catalog_url = function_catalog_url([os_name])
+  catalog_url = catalog_url([os_name])
   "^/index(.*)\.sucatalog$ #{catalog_url}$1.sucatalog [L]"
 end
 
