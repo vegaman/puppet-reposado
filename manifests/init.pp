bec3f202943db1e8b6a21e892355fd34e73c257a
Gerard Kok <gerard@gerardkok.nl>
2015-07-05 14:57:46 +0200

    attempting a cleaner solution to specify apple catalogs

--- manifests/init.pp
+++ manifests/init.pp
@@ -29,6 +29,7 @@ class reposado (
   $metadata_dir = "${base_dir}/metadata"
   $document_root = "${base_dir}/html"
   $local_catalog_url_base = "http://${server_name}"
+  $catalog_urls = catalog_urls($apple_catalogs)
 
   if $manage_group {
     group { $group: ensure => 'present'; }
