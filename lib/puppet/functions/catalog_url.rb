5c49532338bdab17da52ecc9cda12a78be34b15c
Gerard Kok <gerard@gerardkok.nl>
2017-11-12 14:07:37 +0100

    converting functions to the modern Ruby API

---
+++ lib/puppet/functions/catalog_url.rb
@@ -0,0 +1,44 @@
+Puppet::Functions.create_function(:catalog_url) do
+  dispatch :catalog_url do
+    param 'String', :os_name
+    return_type 'String'
+  end
+
+  def os_name_in_catalog_url(os_minor_version)
+    case os_minor_version
+    when 5
+      'leopard'
+    when 6
+      'snowleopard'
+    when 7
+      'lion'
+    when 8
+      'mountainlion'
+    when 9
+      '10.9'
+    when 10
+      '10.10'
+    when 11
+      '10.11'
+    when 12
+      '10.12'
+    else
+      '10.13'
+    end
+  end
+
+  def catalog_url(os_name)
+    case os_name
+    when '10.4'
+      "/content/catalogs/index"
+    when '10.6', 'snowleopard'
+      # doesn't follow the usual pattern
+      "/content/catalogs/others/index-leopard-snowleopard.merged-1"
+    else
+      os_minor_version = call_function('os_minor_version', os_name)
+      os_minor_versions = (5..os_minor_version).to_a.reverse
+      url = os_minor_versions.map { |o| os_name_in_catalog_url(o) }.join('-')
+      "/content/catalogs/others/index-#{url}.merged-1"
+    end
+  end
+end
---
+++ lib/puppet/functions/rewrites.rb
@@ -0,0 +1,25 @@
+Puppet::Functions.create_function(:rewrites) do
+  dispatch :rewrites do
+    param 'Array[String]', :catalogs
+    return_type 'Array[Hash[String, String]]'
+  end
+
+  def rewrite_cond(os_name)
+    darwin_version = call_function('os_minor_version', os_name) + 4
+    "%{HTTP_USER_AGENT} Darwin/#{darwin_version}"
+  end
+
+  def rewrite_rule(os_name)
+    catalog_url = call_function('catalog_url', os_name)
+    "^/index(.*)\.sucatalog$ #{catalog_url}$1.sucatalog [L]"
+  end
+
+  def rewrite(os_name)
+    { "rewrite_cond" => rewrite_cond(os_name), "rewrite_rule" => rewrite_rule(os_name) }
+  end
+
+  def rewrites(catalogs)
+    os_names = catalogs.empty? ? (4..13).to_a.map{ |o| "10.#{o}" } : catalogs
+    os_names.map { |o| rewrite(o) }
+  end
+end
